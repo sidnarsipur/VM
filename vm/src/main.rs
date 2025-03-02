d08ced07803585eb0302a72e5fd59c7390bc494f
sidnarsipur <sid.narsipur@gmail.com>
2025-03-01 20:59:24 -0500

    Add page assignment

--- vm/src/main.rs
+++ vm/src/main.rs
@@ -19,11 +19,11 @@ impl Memory {
         Self { data: [0; 65536] }
     }
 
-    fn read(&self, address: u16) -> u8 {
+    fn load(&self, address: u16) -> u8 {
         self.data[address as usize]
     }
 
-    fn write(&mut self, address: u16, value: u8) {
+    fn store(&mut self, address: u16, value: u8) {
         self.data[address as usize] = value;
     }
 }
@@ -32,18 +32,33 @@ struct PageTable {
     data: [u8; 2097152] //21-bit virtual memory
 }
 
+//TODO: move to an MMU impl?
 impl PageTable {
     fn new() -> Self {
         //Default PTE is 0 for valid bit and 0000 for frame number
         Self { data: [0; 2097152]}
     }
 
+    //Assign a frame to a virtual page
+    fn assign_page(&mut self, virtual_page_number: u32, frame_number: u8) {
+        let valid_bit: u8 = 1;
+        let dirty_bit: u8 = 0;
+
+        let pt_entry = (valid_bit << 5) | (dirty_bit << 6) | (frame_number);
+
+        self.data[virtual_page_number as usize] = pt_entry;
+    }
+
+    fn evict_page() {
+        //TODO: implement LRU approximation (maybe reference bit algorithm?)
+    }
+
     fn is_valid(&self, virtual_address: u32) -> bool {
         //Want upper 21 bits from the virtual address
         // 1111 1111 1111 1111 1111 1000 0000 0000
         // F    F    F    F    F    8    0    0
-        let page_number: u32 = (virtual_address & 0xFFFFF800) >> 11;
-        let pt_entry: u8 = self.data[page_number as usize];
+        let virtual_page_number: u32 = (virtual_address & 0xFFFFF800) >> 11;
+        let pt_entry: u8 = self.data[virtual_page_number as usize];
         
         //Valid bit is 3rd bit
         //0010 0000
@@ -52,20 +67,19 @@ impl PageTable {
         return valid_bit == 1
     }
 
-    //TODO: move to an MMU impl?
     fn get_physical_address(&self, virtual_address: u32) -> u16 {
         if !self.is_valid(virtual_address) {
             //TODO: handle page fault
             return 0;
         }
 
-        let page_number: u32 = (virtual_address & 0xFFFFF800) >> 11;
+        let virtual_page_number: u32 = (virtual_address & 0xFFFFF800) >> 11;
         //Want upper 11 bits from the virtual address
         // 0000 0000 0000 0000 0000 0111 1111 1111
         // 0    0    0    0    0    E    F    F
         let offset: u32 = virtual_address & 0x00000EFF;
 
-        let pt_entry: u8 = self.data[page_number as usize];
+        let pt_entry: u8 = self.data[virtual_page_number as usize];
         //Want lower 5 bits
         //0001 0000
         //1    0
@@ -81,7 +95,7 @@ impl PageTable {
 fn main() {
     let mut mem = Memory::new();
 
-    mem.write(0x1234,42);
+    mem.store(0x1234,42);
 
-    println!("Value is: {}", mem.read(0x1234));
+    println!("Value is: {}", mem.load(0x1234));
 }
